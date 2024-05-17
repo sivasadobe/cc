21580bf0ee6b7ffde10ca00ab79225fafe88276d
Siva S <163842332+sivasadobe@users.noreply.github.com>
2024-05-17 12:01:22 +0530

    Update utils.js

--- creativecloud/scripts/utils.js
+++ creativecloud/scripts/utils.js
@@ -36,7 +36,7 @@ export const [setLibs, getLibs] = (() => {
       }
       const branch = new URLSearchParams(window.location.search).get('milolibs') || 'main';
       if (branch === 'local') { libs = 'http://localhost:6456/libs'; return libs; }
-      if (branch.indexOf('--') > -1) { libs = `https://${branch}.hlx.live/libs`; return libs; }
+      if (branch.indexOf('--') > -1) { libs = `https://${branch}.hlx.page/libs`; return libs; }
       libs = `https://${branch}--milo--adobecom.hlx.live/libs`;
       return libs;
     }, () => libs,
